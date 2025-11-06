6b5e0effe7c1930cae621dc7725c7faa297c62e5
arieffadhillah9-cpu <arieffadhillah9@gmail.com>
2025-11-06 17:12:19 +0700

    Adjust JS

--- resources/views/layout/partials/script.blade.php
+++ resources/views/layout/partials/script.blade.php
@@ -2,6 +2,5 @@
       <script src="{{ asset('adminlte/plugins/jquery/jquery.min.js') }}">
       <script src="{{ asset('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}">
       <!-- AdminLTE App -->
-      <script src="{{ asset('adminlte/dist/js/adminlte.min.js') }}">
-      
+      <script src="{{ asset('adminlte/dist/js/adminlte.min.js') }}"> 
       @stack('scripts')
